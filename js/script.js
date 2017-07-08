1cdfbb94497667d49034c31e9e60df3b5b1d1ea1
zafarullahjoiya <zafar@najeebmediagroup.com>
2017-07-07 22:27:53 -0700

    new html class backup files and created new respository

---
+++ js/script.js
@@ -0,0 +1,25 @@
+jQuery(document).ready(function($) {
+
+		$('.owl-carousel').owlCarousel({
+	    loop:true,
+	    margin:10,
+	    responsiveClass:true,
+	    nav:false,
+	    responsive:{
+	        0:{
+	            items:1,
+	            
+	        },
+	        600:{
+	            items:3,
+	            
+	        },
+	        1000:{
+	            items:5,
+	            
+	            loop:false
+	        }
+	    }
+	})
+
+});
